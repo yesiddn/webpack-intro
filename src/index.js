c2b8aaeab10e3f6f8d1d65c67799006da045b18d
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-05 17:52:41 -0500

    Prepare project structure

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data =
+{
+  name: "Duvan Yesid",
+  nickname: "yesiddn",
+  description: "Vivo para descomponer problemas complejos y crear soluciones con un impacto funcional y positivo. Me apasiona comprender a las personas para crear soluciones valiosas, funcionales y positivas.",
+  avatar: "https://yesiddn.me/assets/img/profile.jpg",
+  social:
+    [
+      {
+        name: "platzi",
+        url: "https://platzi.com/p/",
+        username: "yesiddn",
+      },
+      {
+        name: "github",
+        url: "https://github.com/",
+        username: "yesiddn",
+      },
+      {
+        name: "twitter",
+        url: "https://twitter.com/",
+        username: "yesiddn",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "yesiddn",
+      },
+      {
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/",
+        username: "yesiddn",
+      },
+    ],
+
+  links:
+    [
+      {
+        name: "portfolio",
+        url: "https://yesiddn.me/",
+        color: "red",
+        emoji: "📔",
+      },
+    ],
+  footer: "Made with Love on Colombia",
+};
